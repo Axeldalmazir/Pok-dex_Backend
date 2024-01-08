1ce9908915e5e9a56cbc9d1b4b2de84fcfa88231
--add <axeldalmazirpro@gmail.com>
2024-01-08 18:03:18 -0500

    Correction of the error handling

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -1,20 +1,20 @@
 const { Pokemon } = require('../db/sequelize')
-  
+
 module.exports = (app) => {
-  app.get('/api/pokemons/:id', (req, res) => {
+  app.get('/api/pokemons/:id', auth, (req, res) => {
     Pokemon.findByPk(req.params.id)
-      .then(pokemon = {
+      .then(pokemon => {
         if(pokemon === null) {
-          const message = 'Un pokémon demandé n\'existe pas. Réesayez avec un autre identifiant.';
-          return res.status(404).json({message})
-      }
-      const message = 'Un pokémon a bien été trouvé.'
-      res.json({message, data: pokemon })
-    })
-    .catch(error => {
-      const message = 'Le pokemon n\'a pas pu etre récupéré. Réessayez dans quelques instants.'
-      res.status(500).json({message, data: error})
-    })
+          const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
+          return res.status(404).json({ message })
+        }
+
+        const message = 'Un pokémon a bien été trouvé.'
+        res.json({ message, data: pokemon })
+      })
+      .catch(error => {
+        const message = `Le pokémon n'a pas pu être récupéré. Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
+      })
   })
 }
-
